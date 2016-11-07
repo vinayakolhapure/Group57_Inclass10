dbb6525d5518dd5a38429ed6280bd0f09f0921f7
vinayakolhapure <vinayakolhapure@aol.com>
2016-11-07 17:48:55 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/vinayak/group57_inclass10/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.vinayak.group57_inclass10;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
